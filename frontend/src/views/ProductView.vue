ebfa0c1ee423fe52e7850b93abb07443d4f76171
Purin <purin.peera@mail.kmutt.ac.th>
2026-01-11 07:11:28 +0700

    Update: App's Logo

--- frontend/src/views/ProductView.vue
+++ frontend/src/views/ProductView.vue
@@ -8,13 +8,14 @@ import {
 <template>
   <nav class="bg-white border-b border-gray-100 sticky top-0 z-50">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="flex justify-between h-14 items-center">
+      <div class="flex justify-center sm:justify-between h-14 items-center">
         <div class="flex items-center gap-2">
           <div class="w-8 h-8 rounded-lg flex items-center justify-center">
-            <IconPackage size="32" color="black" />
+            <!-- <IconPackage size="32" color="black" /> -->
+            <img src="/logo.svg" alt="">
           </div>
-          <span class="text-sm md:text-xl font-bold tracking-tight text-gray-900">
-            PRODUCT MANAGEMENT <span class="text-gray-400 font-light">SYSTEM</span>
+          <span class="text-md md:text-xl font-bolder tracking-tight text-gray-900">
+            PRODUCT MANAGEMENT <span class="text-gray-400 font-bold">SYSTEM</span>
           </span>
         </div>
       </div>
